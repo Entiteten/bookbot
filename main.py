7e8c355f606496c6f3bd125acdc83ee6b8c20f38
Entiteten <sebastianlindgren@outlook.com>
2025-07-12 16:49:19 +0200

    B: word count added

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     filepath = "./books/frankenstein.txt"
-    print(get_book_text(filepath))
+    text = get_book_text(filepath)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def word_count(text):
+    words = text.split()
+    return len(words)
 
 main()
