1179273c0c6838a8259f73da78034d906e0d1acb
Entiteten <sebastianlindgren@outlook.com>
2025-07-12 23:34:22 +0200

    D: stats character sorting and main print out

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from stats import word_count
+from stats import word_count, chars_dict, sort_chars
 def main():
     filepath = "./books/frankenstein.txt"
     text = get_book_text(filepath)
     num_words = word_count(text)
-    print(f"{num_words} words found in the document")
+    characters = chars_dict(text)
+    sorted = sort_chars(characters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,36 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def chars_dict(text):
+    chars = {}
+    #lowered = text.lower()
+    for lowered in text.lower():
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_on(result):
+    return result["num"]
+
+def sort_chars(characters):
+    result = []
+    for char, num in characters.items():
+        if char.isalpha():
+            result.append({"char": char, "num": num})
+    result.sort(reverse=True, key=sort_on)
+    return result
+
+
+
+#def sort_chars(chars_dict):
+    #sorted = list(chars_dict.items())
+    #sorted = []
+    #char = chars_dict.split(0)
+    #num = chars_dict.split(1)
+    #for i in chars_dict:
+    #    sorted.append()
+    #sorted.sort(reverse=True, key=sort_on)
+    #return sorted
