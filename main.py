7e7db95c63191f66d076677b853041290e4bcb31
Entiteten <sebastianlindgren@outlook.com>
2025-07-13 14:36:50 +0200

    E: imported sys function for filepath

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+if not sys.argv[1:]:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 from stats import word_count, chars_dict, sort_chars
 def main():
-    filepath = "./books/frankenstein.txt"
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = word_count(text)
     characters = chars_dict(text)
